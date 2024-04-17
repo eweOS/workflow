132c7d0d038c4234bd106a1ee9b1f54dcc482ac7
Yukari Chiba <i@0x7f.cc>
2024-04-17 19:04:59 +0800

    Update info_pkgs.sh

--- info_pkgs.sh
+++ info_pkgs.sh
@@ -36,5 +36,7 @@ echo $DATA | jq > results/_pkgs.json
 for repo in $(jq -cr '.[] | .Repository' results/_pkgs.json | uniq | xargs); do
   mkdir results/$repo
 done
-jq -cr '.[] | .Repository + "/" + .Name, .' results/_pkgs.json | awk 'NR%2{f=$0".json";next} {print >f;close(f)}'
+pushd results
+jq -cr '.[] | .Repository + "/" + .Name, .' _pkgs.json | awk 'NR%2{f=$0".json";next} {print >f;close(f)}'
+popd
 jq -cr '[.[] | {Name,Version,Repository}]' results/_pkgs.json > results/_pkgs_brief.json
